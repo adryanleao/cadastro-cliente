cd19360fa579cff1376d8ed66b51d787ec04eaec
Adryan Leao <adryanleao@gmail.com.com>
2023-11-01 17:15:33 -0300

    Add Front

---
+++ frontend/.next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/adryanleao/Documents/Projetos/Act Digital/Desafio Pratico/cadastro-cliente/frontend/app/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
---
+++ frontend/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"TisMgmFpTJxoXx3fP9HPZTrlvbVb1owl8NDXfWv9xVk=\"\n}"
