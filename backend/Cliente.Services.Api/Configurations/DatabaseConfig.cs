7b13bea876c57ca4b006dc88b9af9c345db5414e
Adryan Leao <adryanleao@gmail.com.com>
2023-11-02 20:09:06 -0300

    Ajustes Docker Compose

--- backend/Cliente.Services.Api/Configurations/DatabaseConfig.cs
+++ backend/Cliente.Services.Api/Configurations/DatabaseConfig.cs
@@ -8,7 +8,8 @@ public static class DatabaseConfig
     public static void AddDatabaseConfiguration(this IServiceCollection services, IConfiguration configuration)
     {
         if (services == null) throw new ArgumentNullException(nameof(services));
-
+        
+        Console.WriteLine(configuration.GetConnectionString("CadastroClienteConnection"));
         services.AddDbContext<ClienteContext>(options =>
             options.UseSqlServer(configuration.GetConnectionString("CadastroClienteConnection")));
 
